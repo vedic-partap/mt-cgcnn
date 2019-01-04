ba5a72166cf328415161958ff56294908e064e83
Vedic Partap <vedicpartap1999@gmail.com>
2019-01-04 22:30:24 +0530

    add comments to modify.py

--- modify.py
+++ modify.py
@@ -2,15 +2,20 @@ import sys
 from gemmi import cif
 import os
 
+# Directory whose cif data has to be changed 
 directory = '../data/data_86_FE_BG'
 # directory = "data/sample"
+
+# Fraction how much ( e.g. 0.99 mean the final bond length will be ( 1-0.99) * original )
 fraction = 0.99
 mod_dir = directory+'_'+str(fraction)+'/'
 
 if __name__ == '__main__':
+    # make directory if not existed
     if not os.path.exists(mod_dir):
         os.makedirs(mod_dir)
     for file in os.listdir(directory):
+        # read all the files and copy if not cif ( like material_id_hash.csv, id_prop.csv etc.)
         if file.endswith(".cif"):
             print(file)
             doc = cif.read_file(directory+"/"+file)
@@ -21,6 +26,7 @@ if __name__ == '__main__':
             block.set_pair('_cell_length_a',len_a)
             block.set_pair('_cell_length_b',len_b)
             block.set_pair('_cell_length_c',len_c)
+            # saving the modified cif file 
             doc.write_file(mod_dir+file)
         else:
             os.popen('cp ' + directory + "/" + file + ' ' + mod_dir + file)
